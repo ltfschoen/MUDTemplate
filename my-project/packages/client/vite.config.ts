2ffd30d07b306a35a6d76fa1e22cb11c096c0208
Luke Schoen <ltfschoen@gmail.com>
2023-05-17 20:43:19 +1000

    allow all in vite config cors. add more commented out defaults

--- my-project/packages/client/vite.config.ts
+++ my-project/packages/client/vite.config.ts
@@ -8,15 +8,31 @@ export default defineConfig({
     fs: {
       strict: false,
     },
-        cors: {
-          // origin: ["ws://127.0.0.1:8545/", "http://127.0.0.1:8545/", "http://localhost:3000/"],
-          origin: "*",
-          methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-          allowedHeaders: ['Content-Type', 'Authorization'],
-          exposedHeaders: ['Content-Range', 'X-Content-Range'],
-          preflightContinue: true,
-          optionsSuccessStatus: 204
-        },
+    cors: {
+      // origin: ["ws://127.0.0.1:8545/", "http://127.0.0.1:8545/", "http://localhost:3000/"],
+      origin: "*",
+      // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+      methods: "*",
+      // allowedHeaders: ['Content-Type', 'Authorization'],
+      allowedHeaders: "*",
+      credentials: true,
+      // exposedHeaders: ['Content-Range', 'X-Content-Range'],
+      exposedHeaders: "*",
+      // preflightContinue: true,
+      // optionsSuccessStatus: 204
+    },
+    // hmr: {
+    //   clientPort: 8545,
+    //   port: 3000,
+    //   overlay: true,
+    // }
+    // proxy: {
+    //   '/socket.io': {
+    //     target: 'ws://127.0.0.1:8545',
+    //     ws: true,
+    //   },
+    // },
+    // strictPort: false,
   },
   build: {
     target: "es2022",
