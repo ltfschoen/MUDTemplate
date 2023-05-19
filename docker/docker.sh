8f27c55eaed5652719c78dfccc9d515032c5392c
Luke Schoen <ltfschoen@gmail.com>
2023-05-19 20:44:36 +1000

    remove the accidental comments so it actually runs

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-# trap "echo; exit" INT
-# trap "echo; exit" HUP
+trap "echo; exit" INT
+trap "echo; exit" HUP
 
-# PARENT_DIR=$( echo $(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")") )
-# # assign fallback values for environment variables from .env.example incase
-# # not declared in .env file. alternative approach is `echo ${X:=$X_FALLBACK}`
-# source $PARENT_DIR/.env.example
-# source $PARENT_DIR/.env
+PARENT_DIR=$( echo $(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")") )
+# assign fallback values for environment variables from .env.example incase
+# not declared in .env file. alternative approach is `echo ${X:=$X_FALLBACK}`
+source $PARENT_DIR/.env.example
+source $PARENT_DIR/.env
 
-# printf "\n*** Started building Docker container."
-# printf "\n*** Please wait... \n***"
+printf "\n*** Started building Docker container."
+printf "\n*** Please wait... \n***"
 
-# # https://stackoverflow.com/a/25554904/3208553
-# set +e
-# bash -e <<TRY
-#   docker build -f ${PARENT_DIR}/docker/Dockerfile ./
-# TRY
-# if [ $? -ne 0 ]; then
-# 	printf "\n*** Detected error running 'docker build'. Trying 'docker buildx' instead...\n"
-# 	docker buildx build -f {PARENT_DIR}/docker/Dockerfile ./
-# fi
+# https://stackoverflow.com/a/25554904/3208553
+set +e
+bash -e <<TRY
+  docker build -f ${PARENT_DIR}/docker/Dockerfile ./
+TRY
+if [ $? -ne 0 ]; then
+	printf "\n*** Detected error running 'docker build'. Trying 'docker buildx' instead...\n"
+	docker buildx build -f {PARENT_DIR}/docker/Dockerfile ./
+fi
 
-# docker run -it -d \
-# 	--env-file "${PARENT_DIR}/.env" \
-# 	--hostname foundry \
-# 	--name foundry \
-# 	--publish 0.0.0.0:8545:8545 \
-# 	--publish 0.0.0.0:3000:3000 \
-# 	--volume ${PARENT_DIR}:/opt:rw \
-# 	foundry:latest
-# if [ $? -ne 0 ]; then
-#     kill "$PPID"; exit 1;
-# fi
-# CONTAINER_ID=$(docker ps -n=1 -q)
-# printf "\n*** Finished building Docker container ${CONTAINER_ID}.\n\n"
+docker run -it -d \
+	--env-file "${PARENT_DIR}/.env" \
+	--hostname foundry \
+	--name foundry \
+	--publish 0.0.0.0:8545:8545 \
+	--publish 0.0.0.0:3000:3000 \
+	--volume ${PARENT_DIR}:/opt:rw \
+	foundry:latest
+if [ $? -ne 0 ]; then
+    kill "$PPID"; exit 1;
+fi
+CONTAINER_ID=$(docker ps -n=1 -q)
+printf "\n*** Finished building Docker container ${CONTAINER_ID}.\n\n"
 
-# docker ps -a
+docker ps -a
 
 printf "\n*** Entering shell of Docker container...\n"
 docker exec -it foundry /opt/docker/run.sh
